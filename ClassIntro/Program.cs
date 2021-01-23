3fc8a8be51d2d649bf6b1bc16933902c1c0f576e
Ayşe Nur Türker <ayse.turker@metu.edu.tr>
2021-01-23 23:11:18 +0300

    16 Ocak Ders Sonu

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            
+            
+            string adi = "Ayşu";
+            int yas = 25;
+
+            Kurs Kurs1 = new Kurs();
+            Kurs1.KursAdi = "C#";
+            Kurs1.Egitmen = "Engin";
+            Kurs1.İzlenmeOrani = 68;
+
+            Kurs Kurs2 = new Kurs();
+            Kurs2.KursAdi = "Java";
+            Kurs2.Egitmen = "Kerem";
+            Kurs2.İzlenmeOrani = 64;
+
+            Kurs Kurs3 = new Kurs();
+            Kurs3.KursAdi = "Python";
+            Kurs3.Egitmen = "Berkay";
+            Kurs3.İzlenmeOrani = 80;
+
+            Kurs Kurs4 = new Kurs();
+            Kurs4.KursAdi = "C++";
+            Kurs4.Egitmen = "Murat";
+            Kurs4.İzlenmeOrani = 100;
+
+            //Console.WriteLine(Kurs1.KursAdi + " " + Kurs1.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] {Kurs1, Kurs2, Kurs3, Kurs4};
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine (kurs.KursAdi + ": " + kurs.Egitmen);
+            }
+
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int İzlenmeOrani { get; set; }
+    }
+
+
+ 
+
+
+
+
+
+
+
+
+
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety = tip güvenliği
+            //do not repeat yourself
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Oku");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Oku");
+            }
+            else
+            {
+                Console.WriteLine("Eşittir Oku");
+            }
+
+           if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+
+            
+        }
+    }
+}
