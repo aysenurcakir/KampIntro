5f2209b82ba34225647d5aff72102c19af84d692
Ayşe Nur Türker <ayse.turker@metu.edu.tr>
2021-01-30 19:28:04 +0300

    4. Gün Ödev3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriListele(Musteri musteri)
+        {
+            Console.WriteLine("ID: " + musteri.Id);
+            Console.WriteLine("Adı: " + musteri.Adi);
+            Console.WriteLine("Soyadı: " + musteri.Soyadi);
+            Console.WriteLine("-------------");
+        }
+
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " eklendi.");
+            Console.WriteLine("-------------");
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " silindi.");
+            Console.WriteLine("-------------");
+        }
+    }
+
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 123456;
+            musteri1.Adi = "Ayşe Nur";
+            musteri1.Soyadi = "Çakır";
+            
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 654321;
+            musteri2.Adi = "Mehmet Fatih";
+            musteri2.Soyadi = "Çakır";
+            
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 1111;
+            musteri3.Adi = "Ömer Şamil";
+            musteri3.Soyadi = "Çakır";
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Id = 2222;
+            musteri4.Adi = "Ayşe Hümeyra";
+            musteri4.Soyadi = "Çakır";
+
+            Musteri[] Musteriler = new Musteri[] { musteri1, musteri2, musteri3, musteri4 };
+
+            MusteriManager musteriManager = new MusteriManager();
+           
+            foreach (Musteri musteri in Musteriler)
+            {
+                musteriManager.MusteriListele(musteri);
+                musteriManager.MusteriEkle(musteri);
+                musteriManager.MusteriSil(musteri);
+            }
+
+
+
+
+
+
+        }
+    }
+}
